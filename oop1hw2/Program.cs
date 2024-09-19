4c56cb7c3e959a522a489e65f802cc3a20efd07e
dekhtiarenkoOleh <sbirok1232@gmail.com>
2024-09-18 21:47:46 -0400

    Add project files.

---
+++ oop1hw2/Program.cs
@@ -0,0 +1,72 @@
+﻿using Italia;
+using Romania;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ukraine;
+
+namespace Ukraine
+{
+    public class Kyiv
+    {
+        public string Name {  get; set; }
+        public int Population {  get; set; }
+        public Kyiv() 
+        {
+            Name = "Kyiv";
+            Population = 31231233;
+        }
+    }
+}
+namespace Romania
+{
+    public class Bucharest
+    {
+        public string Name {  get; set; }
+        public int population {  get; set; }
+        public Bucharest() 
+        {
+            Name = "Bucharest";
+            population = 12333;
+        }
+    }
+}
+namespace Italia
+{
+    public class Paris
+    {
+        public string Name {  get; set; }
+        public int population {  get; set; }
+        public Paris() 
+        {
+            Name = "Paris";
+            population = 32222;
+        }
+    }
+}
+namespace oop1hw2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kyiv kyiv = new Kyiv();
+            Bucharest bucharest = new Bucharest();
+            Paris paris = new Paris();
+            if(kyiv.Population>bucharest.population&&kyiv.Population>paris.population)
+            {
+                Console.WriteLine("max population in Kyiv");
+            }
+            else if(bucharest.population>kyiv.Population&&bucharest.population>paris.population)
+            {
+                Console.WriteLine("max population in Bucharest");
+            }
+            else
+            {
+                Console.WriteLine("max population in Paris");
+            }
+        }
+    }
+}
